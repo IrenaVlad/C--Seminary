4ef8927522bc78e678f61f671ea24e9ea727b701
IrenaVlad <iriscka@bk.ru>
2022-08-06 22:49:04 +0200

    add Task2

---
+++ Seminar1/Task002/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if(numberA > numberB)
+{
+    Console.Write("max = ");
+    Console.WriteLine(numberA);
+    Console.Write("min = ");
+    Console.WriteLine(numberB);
+}
+else
+{
+    Console.Write("max = ");
+    Console.WriteLine(numberB);
+    Console.Write("min = ");
+    Console.WriteLine(numberA);
+}
