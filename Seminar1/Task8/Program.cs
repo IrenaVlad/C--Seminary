8c6cd41a942d37ad83ad11f3b63779f18a4fb9a3
IrenaVlad <iriscka@bk.ru>
2022-08-06 23:34:37 +0200

    add Task8

---
+++ Seminar1/Task8/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+while(count < N)
+{
+    if(count%2 == 0)
+    {
+        int what = count;
+        Console.WriteLine(what);
+        count = count+1;
+    }
+    else
+    {
+        count = count+1;
+    }
+}
