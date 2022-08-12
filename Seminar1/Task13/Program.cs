e16fd535e9f9f070bf66a2bc6f61ba788fd3bb68
IrenaVlad <iriscka@bk.ru>
2022-08-13 00:03:03 +0200

    add task13

---
+++ Seminar1/Task13/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+number = Math.Abs(number);
+if (number < 100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    string s = Convert.ToString(number);
+    int third = int.Parse(s[2].ToString());
+    Console.WriteLine($"Третья цифра числа: {third}");
+}
+
