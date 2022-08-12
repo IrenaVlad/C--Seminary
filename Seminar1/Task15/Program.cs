65a74badb516213acb9764e0c51ed5f069c9fd3d
IrenaVlad <iriscka@bk.ru>
2022-08-13 00:05:10 +0200

    add task15

---
+++ Seminar1/Task15/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число от 1 до 7");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number == 6 || number == 7)
+{
+    Console.WriteLine("Выходной день");
+}
+else
+{
+    Console.WriteLine("Рабочий день");
+}
+
