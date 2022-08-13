f4e3ceb0857d67678459956248303a73fe9fe994
IrenaVlad <iriscka@bk.ru>
2022-08-13 22:27:20 +0200

    change task10

--- Seminar1/Task10/Program.cs
+++ Seminar1/Task10/Program.cs
@@ -1,5 +1,6 @@
 ﻿Console.WriteLine("Введите трехзначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
+number = Math.Abs(number);
 int a = number/10;
 int b = a%10;
 Console.WriteLine($"{b}");
