bb4d65d12c2fa863006f32e2af519ca865b90686
IrenaVlad <iriscka@bk.ru>
2022-08-13 23:14:05 +0200

    add Task18

---
+++ Seminar1/Task18/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите номер четверти: ");
+int quarter = Convert.ToInt32(Console.ReadLine());
+string []arr = {"x > 0; y > 0", "x < 0; y > 0", "x < 0; y < 0", "x > 0; y < 0"};
+if(quarter >= 1 && quarter <=4)
+{
+    Console.WriteLine(arr[quarter -1]);
+}
+else
+{
+    Console.WriteLine("Такой четверти нет");
+}
