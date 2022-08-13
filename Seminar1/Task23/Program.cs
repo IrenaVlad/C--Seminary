19e3c264428e1211554a8e599b0cfbcfacbf4e4e
IrenaVlad <iriscka@bk.ru>
2022-08-13 13:48:13 +0200

    add task23

---
+++ Seminar1/Task23/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+while(count<=number)
+{
+    Console.WriteLine(count*count*count);
+    count++;
+}
