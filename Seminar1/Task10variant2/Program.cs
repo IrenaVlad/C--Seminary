9b20f9d87f3f390353c1248817a0ef6081e2c562
IrenaVlad <iriscka@bk.ru>
2022-08-13 22:24:58 +0200

    change task10variant2

--- Seminar1/Task10variant2/Program.cs
+++ Seminar1/Task10variant2/Program.cs
@@ -1,5 +1,6 @@
 ﻿Console.WriteLine("Введите трехзначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
+number = Math.Abs(number);
 string s = number.ToString();
 int second = int.Parse(s[1].ToString());
 Console.Write("Вторая цифра числа: ");
