a4b6bed913d09215fe0a90a8c7325f807441986d
IrenaVlad <iriscka@bk.ru>
2022-08-17 14:12:41 +0200

    add task27

---
+++ Seminar1/Task27/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число : ");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] Array = new int[N];
+int sum = 0;
+int i = Array.Length;
+int index = 0;
+while(index<i)
+{
+sum = sum + Array[index];
+index++;
+}
+Console.WriteLine(sum);
