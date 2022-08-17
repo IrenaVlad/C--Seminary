40f6645d7529a2a0c6ff7f5b8e5b94816bbcf6ea
IrenaVlad <iriscka@bk.ru>
2022-08-17 13:33:11 +0200

    add task25

---
+++ Seminar1/Task25/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число А: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+int C = Math.Abs(B);
+int sum = A; 
+int count=2;
+while( count <=C)
+{
+sum = sum * A;
+count++;
+}
+Console.WriteLine(sum);
