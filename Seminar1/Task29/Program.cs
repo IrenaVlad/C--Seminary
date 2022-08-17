0659fef194f8d4f9976969e28050ee244785cdf5
IrenaVlad <iriscka@bk.ru>
2022-08-17 14:01:14 +0200

    add task29

---
+++ Seminar1/Task29/Program.cs
@@ -0,0 +1,15 @@
+﻿int N = 8;
+int min = 0;
+int max = 10;
+int[] Array = (int N, int min, int max);
+{
+    int[] arr = new int[N];
+    for(int i = 0; i < N; i++)
+    {
+        arr [i] = new Random().Next(min, max);
+    }
+    return arr;
+}
+Console.Write();
+Console.WriteLine('[' + string.Join(", ", Array(i, min, max)) + ']');
+Console.WriteLine();
