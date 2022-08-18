d234cd066db5019df292273105e1d9362951ff67
IrenaVlad <iriscka@bk.ru>
2022-08-18 14:09:35 +0200

    change task27

--- Seminar1/Task27/Program.cs
+++ Seminar1/Task27/Program.cs
@@ -1,12 +1,24 @@
-﻿Console.WriteLine("Введите число : ");
-int N = Convert.ToInt32(Console.ReadLine());
-string str = N.ToString();
+﻿// Console.WriteLine("Введите число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// string s = n.ToString();
+// int sum = 0;
+// int i = str.Length;
+// int index = 0;
+// while(str[index] < i)
+// {
+    // sum = sum +str[index];
+    // index++;
+// }
+// Console.WriteLine(sum);
+
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+n = Math.Abs(n);
+string s = Convert.ToString(n);
 int sum = 0;
-int i = str.Length;
-int index = 0;
-while(str[index] < i)
-{
-    sum = sum + str[index];
-    index++;
-}
-Console.WriteLine(sum);
+while (n > 0)
+    {
+        sum = sum + n % 10;
+        n = n / 10;
+    }
+    Console.WriteLine(sum);
