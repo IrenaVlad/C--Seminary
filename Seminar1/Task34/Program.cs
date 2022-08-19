2dee36db13f78394c43e0f76acd68262b762ef3d
IrenaVlad <iriscka@bk.ru>
2022-08-19 23:29:12 +0200

    add task34

---
+++ Seminar1/Task34/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = new int[10];
+Random rand = new Random();
+for(int a = 0; a < array.Length; a++)
+{
+    array[a] = rand.Next(100, 1000);
+}
+Console.WriteLine('[' + string.Join(", ", array) + ']');
+int count = 0;
+for(int a = 0; a < array.Length; a++)
+{
+    if(array[a]%2 ==0)
+    {
+        count=count+1;
+    }
+}
+Console.WriteLine($"Количество четных чисел равно {count}");
