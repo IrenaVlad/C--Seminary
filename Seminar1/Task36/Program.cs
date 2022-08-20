5b0b6fbcf3ea391fde199dca25c9fe9d26ba863d
IrenaVlad <iriscka@bk.ru>
2022-08-20 13:35:47 +0200

    add task36

---
+++ Seminar1/Task36/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = new int[10];
+Random rand = new Random();
+for(int a = 0; a < array.Length; a++)
+{
+    array[a] = rand.Next(0, 100);
+}
+Console.WriteLine('[' + string.Join(", ", array) + ']');
+int sum = 0;
+for(int a=1; a < array.Length; a = a+2)
+{
+    sum = sum + array[a];
+}
+Console.WriteLine($"Сумма равна {sum}");
