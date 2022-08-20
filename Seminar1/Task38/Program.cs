db30227545e16ad6ff7249bdf8827c4fc2c138bd
IrenaVlad <iriscka@bk.ru>
2022-08-20 14:27:23 +0200

    change task38

--- Seminar1/Task38/Program.cs
+++ Seminar1/Task38/Program.cs
@@ -1,2 +1,21 @@
-﻿Console.WriteLine("Введите массив");
+﻿Console.WriteLine("Введите массив вещественных чисел из 5 элементов");
 double[] arr = new double[5];
+int index = 0;
+int max = arr[0];
+if(arr[index] > max)
+{
+    max = arr[index];
+    index++;
+} 
+Console.WriteLine($"max = {max}");
+int index = 0;
+int min = arr[0];
+if(arr[index] < min)
+{
+    min = arr[index];
+    index++;
+} 
+Console.WriteLine($"min = {min}");
+int sum = 0;
+sum = max - min;
+Console.WriteLine(sum);
