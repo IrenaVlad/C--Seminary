5575f36fbfa9a55ac28f01b99a66d4761530e33f
IrenaVlad <iriscka@bk.ru>
2022-08-20 13:24:00 +0200

    add task35

---
+++ Seminar1/Task35/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = new int[10];
+Random rand = new Random();
+for(int a = 0; a < array.Length; a++)
+{
+    array[a] = rand.Next(0, 1000);
+}
+Console.WriteLine('[' + string.Join(", ", array) + ']');
+int count = 0;
+for(int a = 0; a < array.Length; a++)
+{
+    if(10 < array[a] < 100)
+    {
+        count=count+1;
+    }
+}
+Console.WriteLine($"Количество элементов массива в отрезке [10,99] равно {count}");
