bce026f61cfc42b98dc5ec6fe46a32c6f2ab7212
IrenaVlad <iriscka@bk.ru>
2022-08-24 23:28:32 +0200

    add task44

---
+++ Seminar1/Task44/Program.cs
@@ -0,0 +1,24 @@
+﻿int Fibonachi(int N)
+{
+    int result = 0;
+    if(N == 0)
+    {
+        result = 0;
+    }
+    else if(N == 1 || N == 2)
+    {
+        result = 1;
+    }
+    else 
+    {
+        result = Fibonachi(N-1) + Fibonachi(N-2);
+    }    
+    return result;
+};
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+for (int i = 0; i < N; i++)
+{
+    Console.WriteLine(Fibonachi(i));
+}
+
