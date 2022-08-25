39d23928bcc02b55cd9f9d2839cd9ab7aa277fcc
IrenaVlad <iriscka@bk.ru>
2022-08-25 15:04:15 +0200

    add task41

---
+++ Seminar1/Task41/Program.cs
@@ -0,0 +1,10 @@
+﻿int[] array = {1, -7, 567, 89, 223};
+int count = 0;
+for(int i =0; i < 5; i++)
+{
+    if(array[i] > 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine($"Количество положительных чисел равно {count}");
