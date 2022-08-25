7522da056c45bd38eac3f381ac17cde90293a602
IrenaVlad <iriscka@bk.ru>
2022-08-25 13:56:56 +0200

    add task45

---
+++ Seminar1/Task45/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] FillArray(int size, int leftRange, int rigthRange)
+{
+    Random rand = new Random();
+    int[] arr = new int[size];
+    for(int i = 0; i < size; i++)
+    {
+        arr[i] = rand.Next(leftRange, rigthRange+1);
+    }
+    return arr;
+}
+int[] array = FillArray(5,-6,6);
+Console.WriteLine('[' +string.Join(", ", array) +']');
+int[] array2 = new int[5];
+for(int j = 0; j < 5; j++)
+{
+    array2[j] = array[j];
+}
+Console.WriteLine('[' +string.Join(", ", array2) +']');
