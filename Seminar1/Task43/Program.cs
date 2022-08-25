0d80f5cd10e1e914563ee32d5cbe226949d5a8c9
IrenaVlad <iriscka@bk.ru>
2022-08-25 14:43:59 +0200

    add task 43

---
+++ Seminar1/Task43/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите значение b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение 21: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+double x = (b1-b2)/(k2-k1);
+double y = k1*x + b1;
+Console.WriteLine($"{(x, y)}"); 
