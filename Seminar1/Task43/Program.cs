51d81edc36ded7268cacf3d414b773deb8f1d1d1
IrenaVlad <iriscka@bk.ru>
2022-08-25 22:32:50 +0200

    change task43

--- Seminar1/Task43/Program.cs
+++ Seminar1/Task43/Program.cs
@@ -6,6 +6,17 @@ Console.WriteLine("Введите значение b2: ");
 double b2 = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine("Введите значение k2: ");
 double k2 = Convert.ToDouble(Console.ReadLine());
-double x = (b1-b2)/(k2-k1);
-double y = k1*x + b1;
-Console.WriteLine($"Координаты точки пересечения {(x, y)}"); 
+if(k1==k2)
+{
+    Console.WriteLine("Прямые не пересекаются");
+}
+else if(b1==b2)
+{
+    Console.WriteLine("Прямые совпадают друг с другом и с осью y");
+}
+else
+{
+    double x = (b1-b2)/(k2-k1);
+    double y = k1*x + b1;
+    Console.WriteLine($"Координаты точки пересечения {(x, y)}"); 
+}
