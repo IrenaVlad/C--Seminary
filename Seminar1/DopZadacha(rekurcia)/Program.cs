81253ff9b57c719d891318dc1200ff21a774bf60
IrenaVlad <iriscka@bk.ru>
2022-08-26 22:53:23 +0200

    add DopZadacha(rekurcia)

---
+++ Seminar1/DopZadacha(rekurcia)/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int Factorial( int N)
+{
+    if(N== 1) return 1;
+    else return N*Factorial(N-1);
+}
+Console.WriteLine(Factorial(N));
