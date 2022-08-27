9072eaac6888f65710d21a8fd924174c5d7a94e1
IrenaVlad <iriscka@bk.ru>
2022-08-27 22:25:52 +0200

    change task47

--- Seminar1/Task52/Program.cs
+++ Seminar1/Task52/Program.cs
@@ -3,13 +3,17 @@ Console.WriteLine("Введите m ");
 int m = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите n ");
 int n = Convert.ToInt32(Console.ReadLine());
-double[,] matrix = new double [m,n];
+int [,] matrix = new int [m,n];
 for(int i = 0; i < m; i++)
 {
-    for(int j = 0; j < n; j++)
+    for(int j = 0; j < 0; j++)
     {
-        matrix[i,j] = new Random().NextDouble();
+        matrix[m,n] = new Random().Next(-10, 10);
         Console.Write(matrix[i,j] + " ");
     }
     Console.WriteLine();
+}
+for(int i =0; i<n; i++)
+{
+
 }
