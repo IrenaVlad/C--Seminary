45e1c28b7211626fe68c91b6c7c4b9fbdafe7ddf
IrenaVlad <iriscka@bk.ru>
2022-08-27 23:27:06 +0200

    add task52

--- Seminar1/Task52/Program.cs
+++ Seminar1/Task52/Program.cs
@@ -6,14 +6,20 @@ int n = Convert.ToInt32(Console.ReadLine());
 int [,] matrix = new int [m,n];
 for(int i = 0; i < m; i++)
 {
-    for(int j = 0; j < 0; j++)
+    for(int j = 0; j < n; j++)
     {
-        matrix[m,n] = new Random().Next(-10, 10);
+        matrix[i,j] = new Random().Next(-10, 10);
         Console.Write(matrix[i,j] + " ");
     }
     Console.WriteLine();
 }
-for(int i =0; i<n; i++)
+Console.WriteLine();
+for(int j = 0; j < n; j++)
 {
-
+    double sum = 0;
+    for(int i = 0; i < m; i++)
+    {
+        sum = sum + matrix[i, j];
+    } 
+    Console.WriteLine($"Среднее арифметическое столбца номер {j+1} равно {sum/m}");
 }
