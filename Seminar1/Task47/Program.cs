8385919ba2c4103c7142ce52a59295e6f32b1ca0
IrenaVlad <iriscka@bk.ru>
2022-08-27 22:09:59 +0200

    add task47

---
+++ Seminar1/Task47/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Задайте двумерный массив  m x n ");
+Console.WriteLine("Введите m ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите n ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] matrix = new double [m,n];
+Random rand = new Random();
+for(int i = 0; i < m; i++)
+{
+    for(int j = 0; j < 0; j++)
+    {
+        matrix[m,n] = rand.NextDouble();
+        Console.Write($"{matrix[i,j]}  ");
+    }
+    Console.WriteLine();
+}
