8dd2bafe13dccb06634d1ae1e7de3b99cb78257e
IrenaVlad <iriscka@bk.ru>
2022-09-03 21:40:44 +0200

    add task60

---
+++ Seminar1/Task60/Program.cs
@@ -0,0 +1,17 @@
+﻿int[,,] array = new int[2,2,2];
+int count = 0;
+Random rand = new Random();
+for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            for(int a = 0; a < array.GetLength(2); a++)
+            {
+                array[i,j,a] = rand.Next(10, 100);
+                Console.Write($"{array[i,j,a]}({i},{j},{a}) ");
+                count++;
+            }
+        }
+        Console.WriteLine();
+    }
+
