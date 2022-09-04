195d8cfe6d959a92dd6cb9fafc3d73e4b98ed7af
IrenaVlad <iriscka@bk.ru>
2022-09-04 23:24:29 +0200

    add task66(false)

---
+++ Seminar1/Task66/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число M ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+void Sum(int M, int N, int sum = 0)
+{
+    if(M == N)
+    {
+        return;
+    }
+    Sum(M = M + 1);
+    sum = sum + M;
+    Console.WriteLine(sum);
+}
+Sum(M,N);
