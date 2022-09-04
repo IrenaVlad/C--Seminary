c653b12fae57e719fd1a82ae39cecfa962072c2d
IrenaVlad <iriscka@bk.ru>
2022-09-04 22:48:19 +0200

    add task64

---
+++ Seminar1/Task64/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void PrintNumbers(int number)
+{
+    if(number == 0)
+    {
+        return;
+    }
+    Console.Write($"{number}  ");
+    PrintNumbers(number-1);
+}
+
+PrintNumbers(n);
