2d5f4e541e091f19eaa0eaf5539e3203fbdbf615
IrenaVlad <iriscka@bk.ru>
2022-09-05 13:54:25 +0200

    change task66

--- Seminar1/Task66/Program.cs
+++ Seminar1/Task66/Program.cs
@@ -2,15 +2,19 @@
 int M = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите число N ");
 int N = Convert.ToInt32(Console.ReadLine());
-
-void Sum(int M, int N, int sum = 0)
+int Sum(int M, int N)
 {
-    if(M == N)
+    if(M > N) 
     {
-        return;
+        return 0;
     }
-    Sum(M = M + 1);
-    sum = sum + M;
-    Console.WriteLine(sum);
+    return M + Sum(M+1,N);
 }
-Sum(M,N);
+if(M < N)
+{
+    Console.WriteLine($"Сумма натуральных чисел в промежутке от M до N равна {Sum(M, N)}");
+}
+else
+{
+    Console.WriteLine($"Сумма натуральных чисел в промежутке от N до M равна {Sum(N, M)}");
+}
---
+++ Seminar1/Task68/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите число m ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число n ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int Ack(int m, int n)
+{
+  if (m == 0)
+  {
+    return n + 1;
+  } 
+  else if (n == 0) 
+  {
+    return Ack(m - 1, 1);
+  }
+  else 
+  {
+    return Ack(m - 1, Ack(m, n - 1));
+    }
+}
+
+Ack(m,n);
